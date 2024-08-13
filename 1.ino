71c2ec7c265279a055cbfcf14f19f12288749caa
Nerya253 <neryalevi253@gmail.com>
2024-08-13 22:02:43 +0300

    התחלה
    מיקום הכפתורים הלדים והבאזר בארדואינו

---
+++ 1.ino
@@ -0,0 +1,12 @@
+#define pinBuzzer 5
+
+#define pinLed_G 6
+#define pinLed_Y 9
+#define pinLed_B 11
+#define pinLed_R 10
+                              
+#define pinBtn_G 4
+#define pinBtn_Y 2
+#define pinBtn_B 3
+#define pinBtn_R 12
+
