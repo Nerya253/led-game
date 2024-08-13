93992b5c8eed970673cd7eff74c5ac989d539bf9
Nerya253 <neryalevi253@gmail.com>
2024-08-13 22:04:49 +0300

    הגדרת מערכים
    הכנסת כל הכפתורים והלדים לתוך מערך וקביעת אורך המערך

--- 1.ino
+++ 1.ino
@@ -10,3 +10,7 @@
 #define pinBtn_B 3
 #define pinBtn_R 12
 
+#define NUM_OF_LEDS 4
+
+int Leds[NUM_OF_LEDS] = {pinLed_G, pinLed_Y, pinLed_B, pinLed_R};
+int Btns[NUM_OF_LEDS] = {pinBtn_G, pinBtn_Y, pinBtn_B, pinBtn_R};
